4d8023a6609ee18ed141192bd069ef479056c6b1
MehdiBits <mehdi.boussaa1@gmail.com>
2025-05-21 15:01:58 +0200

    added dependencies

--- src/geolocation/config.py
+++ src/geolocation/config.py
@@ -12,8 +12,6 @@ INPUT_DIM = 768      # Feature vector dimension from CLIP
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-#MODEL_PATH = "geolocation/src/ressources/no_std_14-10(lvl8).pth"  # Path to classifier weights
-#MODEL_PATH = "geolocation/src/ressources/model_epoch_240.pth" # Path to localizable model weights
-MODEL_PATH = "geolocation/src/ressources/model_epoch_480_loc.pth" # Path to localizable model weights
-PRECOMPUTED_FEATURES_PATH = "geolocation/src/ressources/all_features_from_batches(concat).npy"
-PRECOMPUTED_METADATA_PATH = "geolocation/src/ressources/index_14-10.csv"
+MODEL_PATH = "geolocation/src/geolocation/ressources/no_std_14-10(lvl8).pth"  # Path to classifier weights
+PRECOMPUTED_FEATURES_PATH = "geolocation/src/geolocation/ressources/all_features_from_batches(concat).npy"
+PRECOMPUTED_METADATA_PATH = "geolocation/src/geolocation/ressources/index_14-10.csv"
